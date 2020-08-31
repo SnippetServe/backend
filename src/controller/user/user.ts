45a60020f55b1633ff292716dbb6c785d0c5de04
DevHyperCoder <arduinoleo88@gmail.com>
2020-08-31 11:01:33 +0530

    ✨ Added edit user

--- src/controller/user/user.ts
+++ src/controller/user/user.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import argon2 from 'argon2';
 import User from '../../entities/User';
 
 const router = express.Router();
@@ -33,6 +34,45 @@ router.get('/id', async (req: express.Request, res: express.Response) => {
   });
 
   // TODO uncomment once front end is able to make request
+  // if(req.session.userId == user.uniqueid) {
+  //   res.send(user)
+  // }
+});
+
+// Edit User
+router.put('/', async (req: express.Request, res: express.Response) => {
+  const { body } = req;
+  const { uuid } = body;
+  const user = await User.findOne({ uniqueid: uuid });
+
+  if (!user) {
+    res.send('user not found');
+  }
+
+  const { username } = body;
+  const { password } = body;
+  const { description } = body;
+  const { email } = body;
+
+  if (password.length <= 2) {
+    res.send('Password too short');
+  }
+
+  const hashedPassword = await argon2.hash(password);
+  // c48d4fa5-e147-4651-b4e5-c5bbc21b5d9b
+
+  user.username = username;
+  user.description = description;
+  user.password = hashedPassword;
+  user.email = email;
+  user.save();
+
+  res.send('updated');
+
+  // TODO uncomment once front end is able to make request
+  // if(req.session.userId == user.uniqueid) {
+  //   res.send(user)
+  // }
 });
 
 module.exports = router;
